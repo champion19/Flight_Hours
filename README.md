f114dcaf733e181dc38cca30156bb11bd3731775
champion19 <emmalondogo@gmail.com>
2025-01-06 11:10:30 -0500

    updates en mai y test creadon

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # flutter_santi_app
 
-A new Flutter project that we are going to use to improve our git and flutter skills!
+A new Flutter project that we are going to use to improve our git and flutter skills.
 
---
+++ test/main_app_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_santi_app/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('MainApp has a message', (WidgetTester tester) async {
+    // Build the MainApp widget
+    await tester.pumpWidget(const MainApp());
+
+    // Create the Finders
+    final textFinder = find.text('Hey World,I am  fucking different!');
+
+    // Use the `findsOneWidget` matcher to verify that the text is found
+    expect(textFinder, findsOneWidget);
+  });
+}
