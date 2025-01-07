4b2bcb382bec4a690ae9c3e47fdefd1846a3e897
champion19 <emmalondogo@gmail.com>
2025-01-07 16:16:02 -0500

    refactor: Prueba del hook pre-push #7

--- test/main_app_test.dart
+++ test/main_app_test.dart
@@ -8,7 +8,7 @@ void main() {
 
     // Create the Finders
     final textFinder = find.text(
-        'Hey World,I am  fucking different and we are not all the same every day of our lives!!');
+        'Hey World,I am  fucking different and we are not all the same every day of our lives!!!');
 
     // Use the `findsOneWidget` matcher to verify that the text is found
     expect(textFinder, findsOneWidget);
