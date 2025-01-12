90cbac3b8697f440533c0c17e989976b80952ccc
champion19 <emmalondogo@gmail.com>
2025-01-12 15:41:32 -0500

    refactor: revision exitosa

--- test/main_app_test.dart
+++ test/main_app_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_santi_app/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
   testWidgets('MainApp has a message', (WidgetTester tester) async {
     // Build the MainApp widget
@@ -9,7 +8,7 @@ void main() {
 
     // Create the Finders
     final textFinder = find.text(
-        'Hey World,I am  fucking different and we are not all the same every day of our lives!');
+        'Hey World,I am  fucking different and we are not all the same every day of our lives!!');
 
     // Use the `findsOneWidget` matcher to verify that the text is found
     expect(textFinder, findsOneWidget);
