6fbb18391ba527e87d2c1be7c0999695c6f6425b
champion19 <emmalondogo@gmail.com>
2025-01-13 12:06:38 -0500

    refactor:test upgraded

---
+++ android/app/src/main/kotlin/com/example/flutter_santi_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_santi_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/flutter_santiago/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_santiago
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
