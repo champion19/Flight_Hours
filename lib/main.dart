a93f47a9ee705bc0e5e40980b15ff0c4505b719b
champion19 <emmalondogo@gmail.com>
2025-01-24 16:58:42 -0500

    refactor:flutter native splash added and great

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _SplashScreenState createState() => _SplashScreenState();
 }
 
@@ -46,6 +47,8 @@ class _SplashScreenState extends State<SplashScreen> {
 }
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
